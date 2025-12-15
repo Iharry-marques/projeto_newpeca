fc778af615457b5f4d9944569a5ec8ba0f2e8892
Iharry-marques <fabio.marques.melo@gmail.com>
2025-12-15 14:53:02 -0300

    att

--- backend/config.js
+++ backend/config.js
@@ -2,18 +2,19 @@
 
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'fallback_secret_muito_seguro',
-  
+
   db: {
-    dialect: 'postgres',
-    
+    dialect: process.env.POSTGRES_URL ? 'postgres' : 'sqlite',
+    storage: process.env.POSTGRES_URL ? undefined : './database.sqlite',
+
     // Esta linha diz: "Procure a variável de ambiente POSTGRES_URL"
-    connectionString: process.env.POSTGRES_URL, 
-    
-    dialectOptions: {
+    connectionString: process.env.POSTGRES_URL,
+
+    dialectOptions: process.env.POSTGRES_URL ? {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
-    },
+    } : {},
   }
 };
